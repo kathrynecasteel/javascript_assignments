6d65428a113d297d97bd2d1eb7ec1b4a5017c690
Kathryn Casteel <kathryncasteel@gmail.com>
2016-02-12 18:35:36 -0500

    commit javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var numberOne = 5;
+
+var numberTwo = 6; 
+
+var add = numberOne + numberTwo;
+var subtract = numberOne - numberTwo;
+var multiply = numberOne * numberTwo;
+var divide = numberOne / numberTwo;
+
+
+document.getElementById("plus").innerHTML = "When I add " + numberOne + " to " + numberTwo + " I get " + add ;
+document.getElementById("minus").innerHTML = " When I subtract " + numberOne + " by " + numberTwo + " I get " + subtract ; 
+document.getElementById("times").innerHTML = " When I multiply " + numberOne + " by " + numberTwo + " I get " + multiply ;
+document.getElementById("divi").innerHTML = " When I divide " + numberOne + " by " + numberTwo + " I get " + divide ;
+
+
